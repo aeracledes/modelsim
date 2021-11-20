c978020c991b6cf32c9a3b954054a449ed6d7873
JCD <alexeracledes@gmail.com>
2021-11-20 19:25:45 +0200

    first test mux81 with instantiation

--- LAB2/Exercise1/mux21.v
+++ LAB2/Exercise1/mux21.v
@@ -1,5 +1,6 @@
-module mux21(I0,I1,S,Y);
-input I0,I1,S;
-output Y;
-assign Y = (S&I0)|((~S)*I1);
+module mux21(I0,I1,S0,Y0);
+input I0,I1;
+input S0;
+output Y0;
+assign Y0=S0?I1:I0;
 endmodule
--- LAB2/Exercise1/testmux81.v
+++ LAB2/Exercise1/testmux81.v
@@ -1,24 +1,21 @@
 `timescale 1ns/1ps
 module testmux81; 
-wire out;
-reg A0, A1, A2, A3, A4, A5, A6, A7, A8, S0, S1, S2;
-mux81 CUT1(.A0(A0), .A1(A1), .A2(A2), .A3(A3), .A4(A4), .A5(A5), .A6(A6), .A7(A7), .S0(S0), .S1(S1), .S2(S2), .out(out)); 
-initial 
+wire Y;
+reg [7:0] I;
+reg [2:0] S;
+genvar i;
+mux81 CUT1(I,S,Y);
 begin
-A0=1'b0; A1=1'b0; A2=1'b0; A3=1'b0; A4=1'b0; A5=1'b0; A6=1'b0; A7=1'b0;S0=1'b0; S1=1'b0; S2=1'b0; 
+for (i=0;i<8;i++)
+	I[i] = 1'b0;
+for (i=0;i<3;i++)
+	S[i] = 1'b0;
 #500 $finish; 
 end 
-always #1 A0=~A0;
-always #2 A1=~A1;
-always #3 A2=~A2;
-always #4 A3=~A3;
-always #5 A4=~A4;
-always #6 A5=~A5;
-always #7 A6=~A6;
-always #8 A7=~A7;
-always #9 S0=~S0;
-always #10 S1=~S1;
-always #11 S2=~S2;
-always@(A0 or A1 or A2 or A3 or A4 or A5 or A6 or A7 or S0 or S1 or S2) 
+always #1 for (i=0;i<8;i++)
+	I[i] =~ I[i];
+always #2 for (i=0;i<3;i++)
+	S[i] =~ S[i];
+always@(I[0] or I[1] or I[2] or I[3] or I[4] or I[5] or I[6] or I[7] or S[0] or S[1] or S[2]) 
 $monitor("At time = %t, Output = %d", $time, out); 
 endmodule;
