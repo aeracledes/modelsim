74b128626566daadb79749fa1895cd8d780e0bb1
JCD <alexeracledes@gmail.com>
2021-11-24 00:08:09 +0200

    rewrote mux 81 so that it used instances of mux21

--- LAB2/Exercise1/mux21.v
+++
@@ -1,6 +0,0 @@
-module mux21(I0,I1,S0,Y0);
-input I0,I1;
-input S0;
-output Y0;
-assign Y0=S0?I1:I0;
-endmodule
--- LAB2/Exercise1/testmux81.v
+++ LAB2/Exercise1/testmux81.v
@@ -1,23 +1,18 @@
-testmux81{I,S,Y}
+testmux81(I,S,Y);
+
 mux81 CUT1(I,S,Y);
 initial
 begin
 S=3'b000;I=8'b00000000;
+	#15 S = 3'b000;
+	#15 S = 3'b001;
+	#15 S = 3'b010;
+	#15 S = 3'b011;
+	#15 S = 3'b100;
+	#15 S = 3'b101;
+	#15 S = 3'b110;
+	#15 S = 3'b111;
 
- #15 S=$random ;I=$random;
- #15 S=$random ;I=$random;
- #15 S=$random ;I=$random;
- #15 S=$random ;I=$random;
- #15 S=$random ;I=$random;
- #15 S=$random ;I=$random;
- #15 S=$random ;I=$random;
- #15 S=$random ;I=$random;
- #15 S=$random ;I=$random;
- #15 S=$random ;I=$random;
- #15 S=$random ;I=$random;
- #15 S=$random ;I=$random;
- #15 S=$random ;I=$random;
- #15 S=$random ;I=$random;
 end
 endmodule
 
